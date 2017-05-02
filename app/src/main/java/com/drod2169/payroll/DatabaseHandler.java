23d9ad41b5c9adb0cbeb7f5ee823054f27ef390b
Derek Rodriguez <drod2169@gmail.com>
2017-05-02 05:30:21 -0400

    Code cleanup. Switch to Singleton design. Commented out unused methods for now. Switch hoursWorked to array list, and use gson to parse the array list. Correctly split all array lists after parsing.

--- app/src/main/java/com/drod2169/payroll/DatabaseHandler.java
+++ app/src/main/java/com/drod2169/payroll/DatabaseHandler.java
@@ -10,15 +10,13 @@ import android.util.Log;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
-public class DatabaseHandler extends SQLiteOpenHelper {
+class DatabaseHandler extends SQLiteOpenHelper {
 
     /* TODO: This is messy.. fix this up, make it function better. Possibly two tables, one for
        TODO: employee, one for hours/date.. likely the best route to go.
@@ -48,25 +46,17 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     private ArrayList<String> dates;
     private ArrayList<String> clockIns;
     private ArrayList<String> clockOuts;
+    private ArrayList<Double> hoursWorked;
 
-    private ArrayList<String> mDates;
-    private ArrayList<String> mClockIns;
-    private ArrayList<String> mClockOuts;
-    Gson gson = new Gson();
+    private Gson gson = new Gson();
 
-    String dateInputString;
-    String clockInInputString;
-    String clockOutInputString;
+    private String dateInputString;
+    private String clockInInputString;
+    private String clockOutInputString;
+    private String hoursWorkedGson;
 
-    JSONObject jsonObject1 = new JSONObject();
-    JSONObject jsonObject2 = new JSONObject();
-    JSONObject jsonObject3 = new JSONObject();
 
-    JSONArray datesJson;
-    JSONArray clockInsJson;
-    JSONArray clockOutsJson;
-
-    private static final String[] ALL_KEYS = new String[]{KEY_ID, KEY_NAME, KEY_PAY_RATE, KEY_DATE, KEY_CLOCK_IN, KEY_CLOCK_OUT, KEY_HOURS};
+    //private static final String[] ALL_KEYS = new String[]{KEY_ID, KEY_NAME, KEY_PAY_RATE, KEY_DATE, KEY_CLOCK_IN, KEY_CLOCK_OUT, KEY_HOURS};
 
 
     DatabaseHandler(Context context) {
@@ -94,35 +84,38 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     }
 
 
-    public Cursor getAllRows() {
+   /* public Cursor getAllRows() {
         SQLiteDatabase db = this.getReadableDatabase();
         String where = null;
         Cursor c = db.query(true, TABLE_EMPLOYEE, ALL_KEYS,
-                where, null, null, null, null, null);
+                null, null, null, null, null, null);
         if (c != null) {
             c.moveToFirst();
         }
         return c;
-    }
+    }*/
 
     // Add new employee
-    void addEmployee(Employee employee) {
+    void addEmployee(EmployeeSingleton employee) {
 
         SQLiteDatabase db = this.getWritableDatabase();
 
         dates = employee.getDate();
         clockIns = employee.getClockIn();
         clockOuts = employee.getClockOut();
+        hoursWorked = employee.getWorkedHours();
 
 
         dateInputString = gson.toJson(dates);
         clockInInputString = gson.toJson(clockIns);
         clockOutInputString = gson.toJson(clockOuts);
+        hoursWorkedGson = gson.toJson(hoursWorked);
+
 
         Log.i("Date Input String: ", dateInputString);
 
         ContentValues values = new ContentValues();
-        values.put(KEY_NAME, employee.getEmployeeName());
+        values.put(KEY_NAME, employee.getName());
         values.put(KEY_PAY_RATE, employee.getPayRate());
 
         values.put(KEY_DATE, dateInputString);
@@ -137,28 +130,28 @@ public class DatabaseHandler extends SQLiteOpenHelper {
             values.put(KEY_CLOCK_IN, clockIns.get(i));
             values.put(KEY_CLOCK_OUT, clockOuts.get(i));
         }*/
-        values.put(KEY_HOURS, employee.getHoursWorked());
+        values.put(KEY_HOURS, hoursWorkedGson);
 
-        if (!dbHasData(TABLE_EMPLOYEE, KEY_NAME, employee.getEmployeeName())) {
+        if (!dbHasData(TABLE_EMPLOYEE, KEY_NAME, employee.getName())) {
 
 
-            Log.i("Adding employee: ", employee.getEmployeeName());
+            Log.i("Adding employee: ", employee.getName());
             // Inserting Row
             db.insert(TABLE_EMPLOYEE, null, values);
         } else {
-            db.update(TABLE_EMPLOYEE, values, KEY_ID + "=" + MainActivity.employee.getId(), null);
+            db.update(TABLE_EMPLOYEE, values, KEY_ID + "=" + MainActivity.employees.get(MainActivity.size).getId(), null);
         }
 
         db.close(); // Close database connection
 
     }
 
-    boolean dbHasData(String searchTable, String searchColumn, String searchKey) {
+    private boolean dbHasData(String searchTable, String searchColumn, String searchKey) {
         String query = "Select * from " + searchTable + " where " + searchColumn + " = ?";
         return getReadableDatabase().rawQuery(query, new String[]{searchKey}).moveToFirst();
     }
 
-    // Get single employee
+    /* Get single employee
     public Employee getEmployee(int id) {
 
         SQLiteDatabase db = this.getReadableDatabase();
@@ -166,22 +159,22 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         Cursor cursor = db.query(TABLE_EMPLOYEE, new String[]{KEY_ID,
                         KEY_NAME, KEY_PAY_RATE, KEY_DATE, KEY_CLOCK_IN, KEY_CLOCK_OUT, KEY_HOURS}, KEY_ID + "=?",
                 new String[]{String.valueOf(id)}, null, null, null, null);
+
         Employee employee = null;
-        Type type = new TypeToken<ArrayList<String>>() {
-        }.getType();
         if (cursor != null) {
             cursor.moveToFirst();
 
-            employee = new Employee(Integer.parseInt(cursor.getString(0)),
-                    cursor.getString(1), Double.parseDouble(cursor.getString(2)), cursor.getString(3),
-                    cursor.getString(4), cursor.getString(5), Double.parseDouble(cursor.getString(6)));
+                employee = new EmployeeBuilder().setId(Integer.parseInt(cursor.getString(0))).setName(cursor.getString(1)).setRateOfPay(Double.parseDouble(cursor.getString(2))).setDate0(cursor.getString(3)).setClockIn0(cursor.getString(4)).setClockOut0(cursor.getString(5)).setHoursWorked0(Double.parseDouble(cursor.getString(6))).createEmployee();
+
+            }
+        if (cursor != null) {
+            cursor.close();
         }
         // return employee
-        cursor.close();
         return employee;
-    }
+    }*/
 
-    // Get single employee by name
+    /* Get single employee by name
     public Employee getEmployeeByName(String name) {
 
         SQLiteDatabase db = this.getReadableDatabase();
@@ -191,114 +184,165 @@ public class DatabaseHandler extends SQLiteOpenHelper {
                 new String[]{String.valueOf(name)}, null, null, null, null);
         Employee employee = null;
 
+
         if (cursor != null) {
             cursor.moveToFirst();
 
-            employee = new Employee(Integer.parseInt(cursor.getString(0)),
-                    cursor.getString(1), Double.parseDouble(cursor.getString(2)), cursor.getString(3),
-                    cursor.getString(4), cursor.getString(5), Double.parseDouble(cursor.getString(6)));
+            employee = new EmployeeBuilder().setId(Integer.parseInt(cursor.getString(0))).setName(cursor.getString(1)).setRateOfPay(Double.parseDouble(cursor.getString(2))).setDate0(cursor.getString(3)).setClockIn0(cursor.getString(4)).setClockOut0(cursor.getString(5)).setHoursWorked0(Double.parseDouble(cursor.getString(6))).createEmployee();
+        }
+
+        if (cursor != null) {
+            cursor.close();
         }
         // return employee
-        cursor.close();
         return employee;
-    }
+    }*/
 
-    public Cursor queueAll() {
+    /*public Cursor queueAll() {
         SQLiteDatabase db = this.getReadableDatabase();
         String[] columns = new String[]{KEY_ID, KEY_NAME};
-        Cursor cursor = db.query(TABLE_EMPLOYEE, columns,
-                null, null, null, null, null);
 
-        return cursor;
-    }
+        return db.query(TABLE_EMPLOYEE, columns,
+                null, null, null, null, null);
+    }*/
 
     // Get all employees
     List<Employee> getAllEmployees() {
-        List<Employee> employeeList = new ArrayList<Employee>();
+        List<Employee> employeeList = new ArrayList<>();
         // Select All Query
         String selectQuery = "SELECT  * FROM " + TABLE_EMPLOYEE;
-        Employee employee = new Employee();
+
 
         SQLiteDatabase db = this.getWritableDatabase();
         Cursor cursor = db.rawQuery(selectQuery, null);
         String mDate;
         String mClockIn;
         String mClockOut;
-
-        //Type type = new TypeToken<ArrayList<String>>() {}.getType();
+        String hours;
 
         // looping through all rows and adding to list
         if (cursor.moveToFirst()) {
+            Employee employee;
             do {
-                employee.setID(Integer.parseInt(cursor.getString(0)));
-                employee.setEmployeeName(cursor.getString(1));
-                employee.setPayRate(Double.parseDouble(cursor.getString(2)));
+                int id = Integer.parseInt(cursor.getString(0));
+                String name = cursor.getString(1);
+                Double payRate = Double.parseDouble(cursor.getString(2));
                 mDate = (cursor.getString(3));
                 mClockIn = (cursor.getString(4));
                 mClockOut = (cursor.getString(5));
+                //ArrayList<String> hours = new ArrayList<>();
+                hours = (cursor.getString(6));
+
 
                 Type type = new TypeToken<ArrayList<String>>() {
                 }.getType();
+                Type typeDouble = new TypeToken<ArrayList<Double>>() {
+                }.getType();
+
 
                 dateInputString = gson.toJson(mDate);
                 clockInInputString = gson.toJson(mClockIn);
                 clockOutInputString = gson.toJson(mClockOut);
+                hoursWorkedGson = gson.toJson(hours);
 
-                Log.i("mDate: ", dateInputString);
-                Log.i("mClockIn: ", mClockIn);
-
-                for (String s : dateInputString.split("[\\[\\]\\,\\\\\"]")) {
-                    Log.i("string ", s);
+                String[] splitHours = hoursWorkedGson.split("[\\[\\]\\s*, \"]");
+                String hrReplace = "";
+                for (String splitHr : splitHours) {
+                    hrReplace += splitHr;
                 }
-                String[] splitDate = dateInputString.split("[\\[\\]\\,\\\\\"]");
-                String[] splitClockIn = clockInInputString.split("[\\[\\]\\,\\\\\"]");
-                String[] splitClockOut = clockOutInputString.split("[\\[\\]\\,\\\\\"]");
+                Log.i("hr split ", String.valueOf(splitHours));
+                // String []splitHr2 = hrReplace.split(",");
 
-                List<String> dateSplit = new ArrayList<>();
-                dateSplit.add(splitDate[splitDate.length - 1]);
-                List<String> clockInSplit = new ArrayList<>();
-                clockInSplit.add(splitClockIn[splitClockIn.length - 1]);
-                List<String> clockOutSplit = new ArrayList<>();
-                clockOutSplit.add(splitClockOut[splitClockOut.length - 1]);
-                for (String s : dateSplit) {
-                    Log.i("String split: ", s);
+                String replace1;
+                String replace2 = "";
+                String replClockIn = "";
+
+                String[] splitDate = dateInputString.split("[\\[\\]\\s*, \"]");
+
+                for (String aSplitDate : splitDate) {
+                    replace2 += aSplitDate;
                 }
+                replace1 = replace2.replaceAll("\\\\", ",");
+
+                String[] splitClockIn = clockInInputString.split("[\\[\\]\\s*, \"]");
+                for (String aSplitClockIn : splitClockIn) {
+                    replClockIn += aSplitClockIn;
+                }
+
+                String replClockIn2 = replClockIn.replaceAll("\\\\", ",");
+                String[] splitClockIn2 = replClockIn2.split(",");
+
+                String[] splitClockOut = clockOutInputString.split("[\\[\\]\\s*, \"]");
+                String replClockOut = "";
+
+                for (String aSplitClockOut : splitClockOut) {
+                    replClockOut += aSplitClockOut;
+                }
+                String replClockOut2 = replClockOut.replaceAll("\\\\", ",");
+                String[] splitClockOut2 = replClockOut2.split(",");
+
+                String[] splitDate2 = replace1.split(",");
+
+                List<String> dateSplit = Arrays.asList(splitDate2);
+                List<String> clockInSplit = Arrays.asList(splitClockIn2);
+
+                List<String> hoursSplit = Arrays.asList(splitHours);
+                List<String> clockOutSplit = Arrays.asList(splitClockOut2);
+
                 ArrayList<String> date = new ArrayList<>();
                 ArrayList<String> clockIn = new ArrayList<>();
                 ArrayList<String> clockOut = new ArrayList<>();
+                ArrayList<Double> hour = new ArrayList<>();
 
                 String dateJson = gson.toJson(dateSplit, type);
                 String clockInJson = gson.toJson(clockInSplit, type);
                 String clockOutJson = gson.toJson(clockOutSplit, type);
+                String hoursJson = gson.toJson(hoursSplit, type);
 
-                for (String s : dateSplit) {
-                    date.add(s);
-
+                for (String s : hoursSplit) {
+                    if (!s.isEmpty()) {
+                        hour.add(Double.parseDouble(s));
+                        Log.i("Hr split: ", s);
+                    }
                 }
 
-                Log.i("String split array: ", String.valueOf(date));
+                for (String s : dateSplit) {
+                    if (!s.isEmpty()) {
+                        date.add(s);
+                    }
+                }
 
+                for (String s : clockInSplit) {
+                    if (!s.isEmpty()) {
+                        clockIn.add(s);
+                    }
+                }
+                for (String s : clockOutSplit) {
+                    if (!s.isEmpty()) {
+                        clockOut.add(s);
+                    }
+                }
 
+                // hoursWorked = gson.fromJson(hoursWorkedGson, typeDouble);
                 dates = gson.fromJson(dateJson, type);
                 clockIns = gson.fromJson(clockInJson, type);
                 clockOuts = gson.fromJson(clockOutJson, type);
 
-                Log.i("date from gson array: ", String.valueOf(date));
-
-                employee.setDate(dates);
-                employee.setClockIn(clockIns);
-                employee.setClockOut(clockOuts);
+                try {
+                    employee = new EmployeeBuilder().setId(id).setName(name).setRateOfPay(payRate).setDate(date).setClockIn(clockIn).setClockOut(clockOut).setHoursWorked(hour).createEmployee();
+                    employeeList.add(employee);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
                 String log = "Date: " + String.valueOf(date) + " , Clock in: " + String.valueOf(clockIn) +
                         " , Clock out: " + String.valueOf(clockOut);
                 Log.i("Json conversion: ", log);
 
                 // Adding employee to list
-                employeeList.add(employee);
 
-                employee.setHoursWorked(Double.parseDouble(cursor.getString(6)));
+
             } while (cursor.moveToNext());
         }
-
 
         cursor.close();
         // return employee list
@@ -306,7 +350,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
     }
 
-    // Get employee count
+    /* Get employee count
     public int getEmployeeCount() {
 
         int count;
@@ -319,9 +363,9 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
         return count;
 
-    }
+    }*/
 
-    // Update single employee
+    /* Update single employee
     public int updateEmployee(Employee employee) {
 
         SQLiteDatabase db = this.getWritableDatabase();
@@ -334,18 +378,18 @@ public class DatabaseHandler extends SQLiteOpenHelper {
             values.put(KEY_CLOCK_IN, clockIns.get(i));
             values.put(KEY_CLOCK_OUT, clockOuts.get(i));
         }
-        values.put(KEY_HOURS, employee.getHoursWorked());
+        values.put(KEY_HOURS, String.valueOf(employee.getHoursWorked()));
 
         // Update row
 
         return db.update(TABLE_EMPLOYEE, values, KEY_ID + " = " + employee.getId(),
                 new String[]{String.valueOf(employee.getId())});
 
-    }
+    }*/
 
 
     // Delete single employee
-    public void deleteEmployee(Employee employee) {
+    void deleteEmployee(Employee employee) {
 
         SQLiteDatabase db = this.getWritableDatabase();
         db.delete(TABLE_EMPLOYEE, KEY_ID + " = ?",
@@ -355,7 +399,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
     }
 
-    // Delete single employee by ID
+    /* Delete single employee by ID
     public void deleteEmployeeById(int id) {
 
         SQLiteDatabase db = this.getWritableDatabase();
@@ -364,6 +408,6 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         db.close();
 
     }
-
+*/
 
 }
