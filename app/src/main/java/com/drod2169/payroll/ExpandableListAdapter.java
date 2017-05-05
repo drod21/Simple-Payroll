09ac4b672ba77efb1b6d72a4b79e30775a500f33
Derek Rodriguez <drod2169@gmail.com>
2017-05-05 06:57:48 -0400

    Get rid of null inflater

--- app/src/main/java/com/drod2169/payroll/ExpandableListAdapter.java
+++ app/src/main/java/com/drod2169/payroll/ExpandableListAdapter.java
@@ -48,7 +48,7 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
         if (convertView == null) {
             LayoutInflater infalInflater = (LayoutInflater) this._context
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = infalInflater.inflate(R.layout.list_item, null);
+            convertView = infalInflater.inflate(R.layout.list_item, parent, false);
         }
 
         TextView txtListChild = (TextView) convertView
@@ -86,7 +86,7 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
         if (convertView == null) {
             LayoutInflater infalInflater = (LayoutInflater) this._context
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = infalInflater.inflate(R.layout.list_group, null);
+            convertView = infalInflater.inflate(R.layout.list_group, parent, false);
         }
 
         TextView lblListHeader = (TextView) convertView
